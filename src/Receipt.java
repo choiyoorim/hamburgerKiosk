f0c54fa41b7b772a3f66d0f96cf5ac4f14f81813
Subin Yang <cindy010670@gmail.com>
2023-05-29 20:12:49 +0900

    add: Receipt 클래스 파일 추가

---
+++ src/Receipt.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class Receipt {
+    private HashMap<Menu, Integer> cartInfo = new HashMap<Menu, Integer>();
+    private int totalAmount;
+    private int orderNumber;
+
+    void setCartInfo() {}
+    void setOrderNumber() {}
+    void setTotalAmount() {}
+
+    String print() {
+        return "";
+    }
+}
