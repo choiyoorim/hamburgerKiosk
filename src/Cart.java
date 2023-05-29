acf56e2a3017e4b4c4c19b583a8cc3f4c33fdd69
yangsubinn <cindy010670@gmail.com>
2023-05-29 20:32:24 +0900

    chore: Receipt 생성자 추가, Cart getTotalAmount 함수 추가

--- src/Cart.java
+++ src/Cart.java
@@ -10,6 +10,11 @@ public class Cart {
         return true;
     }
 
+    // Payment에서 requestPayment할때 Cart 클래스 자체를 넘긴다고 했을때, 총 금액 정보만 가져갈 수 있도록 함수 추가
+    int getTotalAmount() {
+        return  totalAmount;
+    }
+
     // 카트에 담긴 항목 보기(없으면 비었다는 문자열, 있으면 카트 항목 가공한 문자열 리턴)
     // '2. 장바구니 보기' 때문에 추가
     String showCartInfo() {
--- src/Receipt.java
+++ src/Receipt.java
@@ -1,9 +1,15 @@
 import java.util.HashMap;
 
 public class Receipt {
-    private HashMap<Menu, Integer> cartInfo = new HashMap<Menu, Integer>();
+    private Cart cartInfo;
     private int totalAmount;
     private int orderNumber;
+
+    Receipt(Cart cartInfo, int totalAmount, int orderNumber) {
+        this.cartInfo = cartInfo;
+        this.totalAmount = totalAmount;
+        this.orderNumber = orderNumber;
+    }
 
     void setCartInfo() {}
     void setOrderNumber() {}
